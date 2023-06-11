a7d0cd3bd2b680e7729132fd09ee3a8e7d613427
Raycia Lima Goncalves <rgoncalves@coakdc.local>
2023-06-11 18:13:37 -0300

    alterações

---
+++ src/Animal.js
@@ -0,0 +1,21 @@
+// Animal.js
+class Animal {
+    constructor(especie) {
+      this.especie = especie;
+    }
+  
+    falar() {
+      console.log(this.especie + " fala");
+    }
+  
+    comer() {
+      console.log(this.especie + " come");
+    }
+  
+    dormir() {
+      console.log(this.especie + " dorme");
+    }
+  }
+  
+  export default Animal;
+  
